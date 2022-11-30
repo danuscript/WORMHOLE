da9e45220b5a7c4e2086b1da71c3ab4a45e4f244
Danny Wallace <contact@dannywallace.dev>
2022-11-30 13:33:40 -0600

    Refactored wormhole class

--- src/Wormhole.js
+++ src/Wormhole.js
@@ -1,13 +1,11 @@
 class Wormhole {
   constructor(el) {
-    this.nodeA = document.createElement('div');
-    this.nodeA.setAttribute('class', 'wormhole');
-    this.nodeA.setAttribute('id', 'wormhole-a');
-    el.appendChild(this.nodeA);
-
-    this.nodeB = document.createElement('div');
-    this.nodeB.setAttribute('class', 'wormhole');
-    this.nodeB.setAttribute('id', 'wormhole-b');
-    el.appendChild(this.nodeB);
+    ['a', 'b'].forEach((label) => {
+      const node = `node${label.toUpperCase()}`;
+      this[node] = document.createElement('div');
+      this[node].setAttribute('class', 'wormhole');
+      this[node].setAttribute('id', `wormhole-${label}`);
+      el.appendChild(this[node]);
+    });
   }
 }
